6d8ba1a5999d487c12eb5c27c97d13a79ec807e7
iamrahultnwar <mr.rahultanwar@gmail.com>
2021-07-03 23:32:54 +0530

    fixed export

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const express = require("express");
 const path = require("path");
 const HydycoAdmin = new express();
 HydycoAdmin.use("/", express.static(path.join(__dirname, "public")));
-export { HydycoAdmin };
+module.exports = { HydycoAdmin };
