1f7ae1a0305f2dd355cd0eaedd610345f1fcf58e
iamrahultnwar <mr.rahultanwar@gmail.com>
2021-07-03 20:54:29 +0530

    missed bracket

--- index.ts
+++ index.ts
@@ -2,7 +2,9 @@ import { App } from "@tinyhttp/app";
 import * as express from "express";
 import * as path from "path";
 const HydycoAdmin = new App();
-const staticServer: any = express.static(path.join(__dirname, "build", "index.html");
+const staticServer: any = express.static(
+  path.join(__dirname, "build", "index.html")
+);
 
 HydycoAdmin.use(staticServer);
 
