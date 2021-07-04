4c6037793e100d5ca43ecb3655f8f8fb0a2ea97f
iamrahultnwar <mr.rahultanwar@gmail.com>
2021-07-04 20:09:10 +0530

    moved to express

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 "use strict";
+const { Router } = require("express");
 const express = require("express");
 const path = require("path");
-const HydycoAdmin = new express();
-HydycoAdmin.use("/", express.static(path.join(__dirname, "public")));
+const HydycoAdmin = Router();
+
+HydycoAdmin.use(express.static(path.join(__dirname, "public")));
+
+HydycoAdmin.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 module.exports = { HydycoAdmin };
