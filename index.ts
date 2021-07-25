1ac09d50243ad530c10c9e240ef4a05a05cce332
Rahul Tanwar <rahul.t@smarter.codes>
2021-07-25 22:40:55 +0530

    fixed css and manifest

--- index.ts
+++ index.ts
@@ -8,6 +8,11 @@ HydycoAdmin.use(
   "/static",
   express.static(path.join(__dirname, "public", "static"))
 );
+HydycoAdmin.use(
+  "/manifest.json",
+  express.static(path.join(__dirname, "public"))
+);
+HydycoAdmin.use("/style.css", express.static(path.join(__dirname, "public")));
 HydycoAdmin.use("/admin-ui/*", express.static(path.join(__dirname, "public")));
 
 HydycoAdmin.use("/admin-ui", (req, res) => res.redirect("/admin-ui/"));
